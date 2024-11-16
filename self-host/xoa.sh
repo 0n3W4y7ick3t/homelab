30df43a2d2e1604639ee4a45511a7cc832235f40
Leon <leonsatoshi39@gmail.com>
2024-11-16 17:46:14 +0900

    change xoa docker's network to host mode, cant connect to xcp-ng host if using bridge mode
    occupied port 80 :(

--- self-host/xoa.sh
+++ self-host/xoa.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-mkdir -p $HOME/.local/share/xoa/xo-server
-mkdir -p $HOME/.local/share/xoa/redis
-
 docker run -itd \
   --name xoa \
-  -p 800:80 \
-  --restart always \
-  -v $HOME/.local/share/xoa/xo-server:/var/lib/xo-server \
-  -v $HOME/.local/share/xoa/redis:/var/lib/redis \
+  --network host \
+  -v xoa-data:/var/lib/xo-server \
+  -v xoa-redis:/var/lib/redis \
+  --restart unless-stopped \
   ronivay/xen-orchestra
