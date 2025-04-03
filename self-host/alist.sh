8bb4f9acbdb42d5237ca6cf98e8ac38550515618
Leon <118800175+0n3W4y7ick3t@users.noreply.github.com>
2025-04-03 18:43:14 +0900

    Update alist.sh

--- self-host/alist.sh
+++ self-host/alist.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 docker run -d \
   --name alist \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e UMASK=022 \
   -v alist-data:/opt/alist/data \
   -v /mnt/share:/opt/alist/data/root \
   -p 5244:5244 \
